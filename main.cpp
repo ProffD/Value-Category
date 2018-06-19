531f09587c00f1c5a3db0766a68cfad8e62010d0
ProffD <tbmatome@gmail.com>
2018-06-19 18:38:12 +0200

    Added lvalue and rvalue references test

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 
 int square(int);
-
+void check(int &value);
+void check(int &&value);
 
 int main() {
 
@@ -21,10 +22,33 @@ int main() {
     std::cout << "&value1: "<< lptr1 <<", its value is: "<< *lptr1<< std::endl;
     std::cout << "&value2: "<< lptr3 <<", its value is: "<< *lptr3<< std::endl;
     std::cout << "&++value2: "<< lptr2 << ", its value is: "<< *lptr2<<std::endl;
+    std::cout << "===========REFERENCES======================"<<std::endl;
+
+    // lvalue references
+    int &value4 = value1;
+    //int &value5 = square(5);       error, cannot bind lvalue reference with rvalues
+    const int &value5 = square(5);   //OK
+    check(value4);
+    check(++value4);
+
+    // rvalue references
+   // int &&value6 = value1;          error, cannot bind rvalue reference with lvalues
+    int &&value6 = square(5);         //OK
+    const int &&value7 = square(5);   //OK
+    check(square(5));
+    check(value6++);
     return 0;
 }
 
 int square(int n)
 {
     return n * n;
+}
+void check(int &value)
+{
+    std::cout<<" lvalue function called with: "<<value <<std::endl;
+}
+void check(int &&value)
+{
+    std::cout<<" rvalue function called with: "<<value <<std::endl;
 }
